9a9bcb8a77c131563fbf2953328627a3a48c3ce5
fatteme khashei <fatt3me@gmail.com>
2022-08-03 16:51:37 +0430

    feat: add is_approved

--- models/user/Admin.py
+++ models/user/Admin.py
@@ -2,5 +2,6 @@ class Admin:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.is_approved = 0
 
 #signup with superadmin permisson, login, logout, strike, video removal, strike removal, stream video
